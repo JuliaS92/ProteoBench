cf0d1f1bd6756f3b63c8ae16a8ef937f5472ed0b
Luxxii <dl94@live.de>
2025-02-05 14:25:25 +0100

    Added AlphaDia-Parsing for the Protein-Module (no tests)

--- proteobench/io/parsing/parse_proteins.py
+++ proteobench/io/parsing/parse_proteins.py
@@ -21,5 +21,7 @@ def load_input_file(input_csv: str, input_format: str) -> pd.DataFrame:
         # Remove the whole filepath in *.pg_matrix and the extension of the filenames
         rename_map = {c: Path(c.replace("/", "\\").split("\\")[-1]).stem for c in input_data_frame.columns[4:]}
         input_data_frame = input_data_frame.rename(columns=rename_map)
+    elif input_format == "AlphaDIA":
+        input_data_frame = pd.read_csv(input_csv, low_memory=False, sep="\t")
 
     return input_data_frame
