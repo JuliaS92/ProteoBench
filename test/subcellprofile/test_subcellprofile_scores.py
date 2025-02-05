9d39c75fa78877ab5e34822541c0badee60e3a81
Julia Schessner <schessner@biochem.mpg.de>
2025-02-05 10:11:36 +0100

    Add run of SpatialDataSetComparison to get complex and reproducibility measures

--- test/subcellprofile/test_subcellprofile_scores.py
+++ test/subcellprofile/test_subcellprofile_scores.py
@@ -3,15 +3,15 @@ import os
 
 from pytest import fixture
 
+from pytest import fixture
+
 from proteobench.score.subcellprofile.subcellprofile_scores import Subcellprofile_Scores
 
 dir = os.path.dirname(__file__)
 
 
-
 @fixture
 def sp_scores():
-    """Fixture to return a Subcellprofile_Scores object with valid data."""
     settings = os.path.join(dir, "../data/subcellprofile/domqc_settings_raw_input.json")
     content = os.path.join(dir, "../data/subcellprofile/pg.matrix.tsv")
     settings = json.load(open(settings))
@@ -19,8 +19,6 @@ def sp_scores():
     sp_scores = Subcellprofile_Scores()
     sp_scores.generate_SpatialDataSet(content, settings)
     return sp_scores
-
-
 
 
 def test_generate_SpatialDataset():
@@ -34,7 +32,5 @@ def test_generate_SpatialDataset():
     assert list(sp_scores.sd.analysed_datasets_dict.keys()) == ["AlphaDIA 1.9.2 Lumos_predicted"]
 
 
-# TODO: Parameterize this for different input formats
-def test_run_SpatialDataSetComparison_noerrors(sp_scores: Subcellprofile_Scores):
-    """Running the SpatialDataSetComparison should not raise errors with any input."""
+def test_run_SpatialDataSetComparison(sp_scores):
     sp_scores.run_SpatialDataSetComparison()
