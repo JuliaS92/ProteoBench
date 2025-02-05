2adea21ad046c8d7127431c38e33e6c86eed54d3
David M. Hollenstein <hollenstein.david@gmail.com>
2025-02-05 11:30:40 +0100

    Black formatting

--- proteobench/io/parsing/parse_proteins.py
+++ proteobench/io/parsing/parse_proteins.py
@@ -30,6 +30,7 @@ def load_input_file(input_csv: str, input_format: str) -> pd.DataFrame:
         input_data_frame = pd.read_csv(input_csv, low_memory=False, sep="\t")
 
         # Remove the whole filepath in *.pg_matrix and the extension of the filenames
+        rename_map = {c: Path(c.replace("/", "\\").split("\\")[-1]).stem for c in input_data_frame.columns[4:]}
         rename_map = {c: Path(c.replace("/", "\\").split("\\")[-1]).stem for c in input_data_frame.columns[5:]}
         input_data_frame = input_data_frame.rename(columns=rename_map)
 
