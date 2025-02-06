33cb12eb962de1bc932630167d94a4a30c2f801f
David M. Hollenstein <hollenstein.david@gmail.com>
2025-02-06 11:17:37 +0100

    Rename Subcellprofile_Scores to Subcellprofile_ProteinScores

--- test/test_subcellprofile/test_subcellprofile_scores.py
+++ test/test_subcellprofile/test_subcellprofile_scores.py
@@ -1,31 +1,33 @@
 import json
 import os
-from unittest.mock import MagicMock, patch
+from unittest.mock import patch
 
 import pandas as pd
 import pytest
 from pytest import fixture
 
-from proteobench.score.subcellprofile.subcellprofile_scores import Subcellprofile_Scores
+from proteobench.score.subcellprofile.subcellprofile_scores import (
+    Subcellprofile_ProteinScores,
+)
 
 dir = os.path.dirname(__file__)
 
 
 @fixture
 def Score_SpatialDataSet():
-    """Fixture to return a Subcellprofile_Scores object with valid data."""
+    """Fixture to return a Subcellprofile_ProteinScores object with valid data."""
     settings = os.path.join(dir, "../data/subcellprofile/domqc_settings_raw_input.json")
     content = os.path.join(dir, "../data/subcellprofile/pg.matrix.tsv")
     settings = json.load(open(settings))
     content = open(content)
-    sp_scores = Subcellprofile_Scores()
+    sp_scores = Subcellprofile_ProteinScores()
     sp_scores.generate_SpatialDataSet(content, settings)
     return sp_scores
 
 
 @fixture
 def Score_SpatialDataSetComparison(Score_SpatialDataSet):
-    """Fixture to return a Subcellprofile_Scores object with valid data and a SpatialDataSetComparison object."""
+    """Fixture to return a Subcellprofile_ProteinScores object with valid data and a SpatialDataSetComparison object."""
     Score_SpatialDataSet.run_SpatialDataSetComparison()
     return Score_SpatialDataSet
 
@@ -36,13 +38,13 @@ def test_generate_SpatialDataset():
     content = os.path.join(dir, "../data/subcellprofile/pg.matrix.tsv")
     settings = json.load(open(settings))
     content = open(content)
-    sp_scores = Subcellprofile_Scores()
+    sp_scores = Subcellprofile_ProteinScores()
     sp_scores.generate_SpatialDataSet(content, settings)
     assert list(sp_scores.sd.analysed_datasets_dict.keys()) == ["AlphaDIA 1.9.2 Lumos_predicted"]
 
 
 # TODO: Parameterize this for different input formats
-def test_run_SpatialDataSetComparison_noerrors(Score_SpatialDataSet: Subcellprofile_Scores):
+def test_run_SpatialDataSetComparison_noerrors(Score_SpatialDataSet: Subcellprofile_ProteinScores):
     """Running the SpatialDataSetComparison should not raise errors with any input."""
     Score_SpatialDataSet.run_SpatialDataSetComparison()
 
@@ -50,13 +52,13 @@ def test_run_SpatialDataSetComparison_noerrors(Score_SpatialDataSet: Subcellprof
 @pytest.mark.skip("requires domaps 1.0.5")
 def test_median_profile_reproducibility(Score_SpatialDataSetComparison):
     """Running the method median_profile_reproducibility and assert the cast of the result."""
-    medians_test = Score_SpatialDataSetComparison.median_profile_reproducibility()
+    medians_test = Score_SpatialDataSetComparison._median_profile_reproducibility()
     assert isinstance(medians_test, float)
 
 
 def test_complex_scatter_unnormalized_noerrors(Score_SpatialDataSetComparison):
     """Test the complex scatter average is calculated correctly."""
-    mean_complex_scatter = Score_SpatialDataSetComparison.complex_scatter_unnormalized()
+    mean_complex_scatter = Score_SpatialDataSetComparison._complex_scatter_unnormalized()
     assert isinstance(mean_complex_scatter, float)
 
 
@@ -72,8 +74,8 @@ def test_get_metrics_dict_keys(mock_aggregate, Score_SpatialDataSetComparison):
     assert set(results.keys()) == set(
         [
             "depth_id_total",
-            "depth_profile_intersection",
-            "depth_id_total",
+            "depth_profile_total",
+            "depth_id_intersection",
             "depth_profile_intersection",
             "median_profile_reproducibility",
             "mean_complex_scatter",
